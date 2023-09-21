edc30733f4a2c6200021beebb88ef39e5282260f
jvelilla <javier.hector@gmail.com>
2023-09-21 20:02:37 -0300

    Updated the WeatherForecast Controller to return an String. Added the CustomApplicationModelProviderFacade to filter unneded features in the Controllers of ASP.NetCOre.

---
+++ cs/ASPNetUtil/ASPNetUtil/CustomApplicationModelProviderFacade.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.Extensions.DependencyInjection;
+using MyFirstapi;
+
+namespace ASPNetUtil;
+public static class CustomApplicationModelProviderFacade
+{
+    public static void Register(IServiceCollection services)
+    {
+        services.AddSingleton<IApplicationModelProvider, CustomApplicationModelProvider>();
+    }
+}
