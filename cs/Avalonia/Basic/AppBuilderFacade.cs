7dceb40f50a240f8e4f110c691b7120a7378ce88
jvelilla <javier.hector@gmail.com>
2023-10-12 11:29:30 -0300

    Added Avalonia Basic Example implemented using a Facade.

---
+++ cs/Avalonia/Basic/AppBuilderFacade.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+using BasicMvvmSample;
+
+public class AppBuilderFacade
+{
+    public AppBuilder BuildAndConfigure()
+    {
+        // Need to create a Facade for AppBuilder.Configure<App>
+        // But AppBuilder doesn't provide a public constructor or 
+        // Factort method without using Generics.
+        return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .UseReactiveUI();
+    }
+}
